b55010eccbb3eb8ce441247529149e722fd2a747
debasish-itobuz <debasish.das@itobuz.com>
2024-04-09 18:01:50 +0530

    feat : game completed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const btnRoll = document.querySelector('.btn--roll');
 const holdRoll = document.querySelector('.btn--hold');
+const newRoll = document.querySelector('.btn--new');
 const dice = document.querySelector('.dice');
 
 let score0 = document.querySelector('#score--0');
@@ -15,6 +18,7 @@ let player = Number(playerScore.textContent);
 let computer = Number(ComputerScore.textContent);
 
 let flag = 0;
+let playing = true;
 
 const diceArr = [
   'dice-1.png',
@@ -26,45 +30,75 @@ const diceArr = [
 ];
 
 const showDice = () => {
-  let random = Math.floor(Math.random() * 6);
-  console.log(random);
-  // dice.src = "dice-1.png";
-  dice.src = diceArr[random];
-  if (flag === 0) {
-    if (random === 0) {
-      player = 0;
-      playerScore.innerText = player;
-      flag = 1;
+  if (playing) {
+    let random = Math.floor(Math.random() * 6);
+    console.log(random);
+    dice.src = diceArr[random];
+    if (flag === 0) {
+      if (random === 0) {
+        player = 0;
+        playerScore.innerText = player;
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+        flag = 1;
+      } else {
+        player += random + 1;
+        playerScore.innerText = player;
+      }
     } else {
-      player += random + 1;
-      // console.log(player);
-      playerScore.innerText = player;
-    }
-  } else {
-    if (random === 0) {
-      computer = 0;
-      ComputerScore.innerText = computer;
-      flag = 0;
-    } else {
-      computer += random + 1;
-      ComputerScore.innerText = computer;
+      if (random === 0) {
+        computer = 0;
+        ComputerScore.innerText = computer;
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+        flag = 0;
+      } else {
+        computer += random + 1;
+        ComputerScore.innerText = computer;
+      }
     }
   }
 };
 
-const holdFunc = ()=>{
-    if(flag === 0){
-        score0.textContent = player;
-        playerScore.textContent = 0
-        // flag = 1
+const holdFunc = () => {
+  if (playing) {
+    if (flag === 0) {
+      scorePlayer += player;
+      score0.textContent = scorePlayer;
+      player = 0;
+      playerScore.textContent = 0;
+      player0.classList.toggle('player--active');
+      player1.classList.toggle('player--active');
+      flag = 1;
+    } else {
+      scoreComputer += computer;
+      score1.textContent = scoreComputer;
+      computer = 0;
+      ComputerScore.textContent = 0;
+      player0.classList.toggle('player--active');
+      player1.classList.toggle('player--active');
+      flag = 0;
     }
-    else{
-        score1.textContent = computer;
-        ComputerScore.textContent = 0
-        // flag = 0
-    }
-}
+  }
+
+  if (scorePlayer >= 20) {
+    playing = false;
+    player0.classList.add('player--winner');
+    player0.classList.remove('player--active');
+    dice.src = '';
+  }
+  if (scoreComputer >= 20) {
+    playing = false;
+    player1.classList.add('player--winner');
+    player1.classList.remove('player--active');
+    dice.src = '';
+  }
+};
 
 btnRoll.addEventListener('click', showDice);
 
-holdRoll.addEventListener('click', holdFunc)
+holdRoll.addEventListener('click', holdFunc);
+
+newRoll.addEventListener('click', ()=>{
+  location.reload();
+})
